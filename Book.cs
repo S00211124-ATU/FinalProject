9b9bc76632552e845fc9e0bbc3584e8e468ea4d7
ohnoSnoopy <S00211124@mail.itsligo.ie>
2022-04-28 19:26:00 +0100

    creating properties

---
+++ Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Book
+    {
+        //properties
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string BookImage { get; set; }
+
+    }
+}
